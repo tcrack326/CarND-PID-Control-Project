bb2f03fd2de9be6ad2399d6caab724689adb3ada
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2017-05-14 21:53:21 -0400

    initial commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,20 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  p_error = 0.0;
+  i_error = 0.0:
+  d_error = 0.0;
+  Kp = Kp;
+  Ki = Ki;
+  Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - p_error;
 }
 
 double PID::TotalError() {
+  return -(p_error * Kp) - (i_error * Ki) - (d_error * Kd);
 }
-
